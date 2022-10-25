a3d2f0dee674e2b005e7cc11dcab4f202fdc452e
kNakkes <112758350+kNakkes@users.noreply.github.com>
2022-10-25 17:32:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,75 +4,68 @@ const cors = require('cors');
 
 const token = "5658832370:AAGGyELJIgkDt_x4zUfzol1fIjF-3N23YBM";
 const webAppUrl = "https://creative-chebakia-ef1bb6.netlify.app";
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-  if (text === "/start") {
-    await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
-      reply_markup: {
-        keyboard: [
-          [{ text: "Заполнить форму", web_app: { url: webAppUrl + "/form" } }],
-        ],
-      },
-    });
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
 
-    await bot.sendMessage(
-      chatId,
-      "Заходи в наш интернет магазин по кнопке ниже",
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Сделать заказ", web_app: { url: webAppUrl } }],
-          ],
-        },
-      }
-    );
-  }
-
-  if (msg?.web_app_data?.data) {
-    try {
-      const data = JSON.parse(msg?.web_app_data?.data);
-      console.log(data);
-      await bot.sendMessage(chatId, "Проверьте правильность данных");
-      await bot.sendMessage(chatId, "Ваше имя: " + data?.country);
-      await bot.sendMessage(chatId, "Ваш номер телефона: " + data?.street);
-
-      setTimeout(async () => {
-        await bot.sendMessage(chatId, "Всю информацию вы получите в этом чате");
-      }, 3000);
-    } catch (e) {
-      console.log(e);
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
     }
-  }
+
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 });
 
-app.post('/web-data', async(req, res) => {
-  const {queryId, products, totalPrice} = req.body;
-  try {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Успешная покупка',
-      input_message_content: {message_text: 'Спасибо за покупку, вы приобрели товар на сумму: ' + totalPrice}
-    })
-    return res.status(200).json({});
-  } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось приобрести товар',
-      input_message_content: {message_text: 'Не удалось приобрести товар'}
-    })
-    return res.status(500).json({});
-  }
-  
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
 })
 
 const PORT = 8000;
-app.listen(PORT, () => console.log('sever started on PORT ' + PORT))
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
